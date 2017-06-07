8381373e353a888839e70a350c6ddbd5addd820c
thomaslawton91 <thomaslawton91@gmail.com>
2017-06-07 16:52:26 -0400

    Create controllers, test, finish Todo API

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -66,6 +66,8 @@ RSpec.configure do |config|
 
   # add 'FactoryGirl' methods
   config.include FactoryGirl::Syntax::Methods
+
+  config.include RequestSpecHelper, type: :request
 
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time
   config.before(:suite) do
